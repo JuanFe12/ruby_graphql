1a7e68cf66670b896c3f7d875db13fd7e8d018f9
Juan Felipe <juanfelipeariastabares007@gmail.com>
2020-10-27 10:57:22 -0500

    settings graphql

---
+++ app/types/query_type.rb
@@ -0,0 +1,24 @@
+QueryType = GraphQL::ObjectType.define do
+    name "Query"
+    description "The query root for this schema"
+  
+    field :movies do
+      type types[MovieType]
+      argument :year, types.Int
+      resolve -> (obj, args, ctx) {
+        if args[:year].present?
+          Movie.where(year: args[:year])
+        else
+          Movie.all
+        end
+      }
+    end
+  
+    field :actor do
+      type ActorType
+      argument :id, !types.ID
+      resolve -> (obj, args, ctx) {
+        Actor.find(args[:id])
+      }
+    end
+  end
